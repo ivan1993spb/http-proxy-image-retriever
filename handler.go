7a4a9fca4f29daac10c1390a1a0388cc150b10bd
Ivan Pushkin <pushkin13@bk.ru>
2016-05-21 14:38:00 +0300

    created graceful server

--- handler.go
+++ handler.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
 type HTTPProxyHandler struct {
 }
 
 func (h *HTTPProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("accepted request")
+	time.Sleep(time.Second * 10)
+	fmt.Println("finished handling")
 
 }
--- main.go
+++ main.go
@@ -1,41 +1,16 @@
 package main
 
 import (
-	"log"
-	"net"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
+	"fmt"
 	"time"
+
+	"gopkg.in/tylerb/graceful.v1"
 )
 
 func main() {
+	graceful.Run(":8888", time.Second, &HTTPProxyHandler{})
 
-	laddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8888")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	listener, err := net.ListenTCP("tcp", laddr)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	stoppableListener, err := NewStoppableListener(listener)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("listening on", stoppableListener.Addr())
-
-	go func() {
-		err := http.Serve(stoppableListener, &HTTPProxyHandler{})
-		log.Println(err)
-	}()
-
-	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	log.Println("ok", <-ch)
-
-	stoppableListener.Stop()
-
-	time.Sleep(time.Second)
+	fmt.Println("test conn")
+	time.Sleep(time.Second * 10)
+	fmt.Println("ok conn")
 }
