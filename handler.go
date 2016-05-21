cb1a2878899fce3ef53f4aa7da05a833c305ec8e
Ivan Pushkin <pushkin13@bk.ru>
2016-05-21 15:48:08 +0300

    created close notifier

--- handler.go
+++ handler.go
@@ -11,7 +11,17 @@ type HTTPProxyHandler struct {
 
 func (h *HTTPProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("accepted request")
-	time.Sleep(time.Second * 10)
-	fmt.Println("finished handling")
 
+	cn, ok := w.(http.CloseNotifier)
+	if ok {
+		select {
+		case <-cn.CloseNotify():
+			fmt.Println("okok")
+		case <-time.After(time.Second * 10):
+		}
+	} else {
+		<-time.After(time.Second * 10)
+	}
+
+	fmt.Println("finished handling")
 }
