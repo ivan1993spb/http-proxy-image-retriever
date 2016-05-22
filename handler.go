d80c219090a59eaf0848d6d1ca72accb3b2f64c8
Ivan Pushkin <pushkin13@bk.ru>
2016-05-22 03:11:08 +0300

    created stop func for handler

--- handler.go
+++ handler.go
@@ -1,62 +1,62 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"sync"
 )
-
-//go:generate go-bindata static/
 
 type HTTPImageProxyHandler struct {
 	logger   *log.Logger
-	stopChan <-chan struct{}
+	stopLock sync.Mutex
+	stopChan chan struct{}
+}
+
+func NewHTTPImageProxyHandler(logger *log.Logger) *HTTPImageProxyHandler {
+	return &HTTPImageProxyHandler{
+		logger:   logger,
+		stopChan: make(chan struct{}),
+	}
 }
 
 func (h *HTTPImageProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println("accepted request")
+	defer log.Println("finished connection handling")
 
-	if r.Method == http.MethodGet {
-		if r.URL.Path == "/" {
-			if url := r.FormValue("url"); url != "" {
-				cn, ok := w.(http.CloseNotifier)
-				var stopChan chan struct{}
-				if ok {
-					stopChan = make(chan struct{})
-					go func() {
-						select {
-						case <-h.stopChan:
-							fmt.Println(1)
-						case <-cn.CloseNotify():
-							fmt.Println(2)
-						}
-						close(stopChan)
-					}()
-				} else {
-					go func() {
-						<-h.stopChan
-						close(stopChan)
-					}()
-				}
+	//cn, ok := w.(http.CloseNotifier)
+	//var stopChan chan struct{}
+	//if ok {
+	//	stopChan = make(chan struct{})
+	//	go func() {
+	//		select {
+	//		case <-h.stopChan:
+	//			fmt.Println(1)
+	//		case <-cn.CloseNotify():
+	//			fmt.Println(2)
+	//		}
+	//		close(stopChan)
+	//	}()
+	//} else {
+	//	go func() {
+	//		<-h.stopChan
+	//		close(stopChan)
+	//	}()
+	//}
+	//
+	//select {
+	//case <-stopChan:
+	//	fmt.Println("okok")
+	//case <-time.After(time.Second * 10):
+	//}
 
-				select {
-				case <-stopChan:
-					fmt.Println("okok")
-				case <-time.After(time.Second * 10):
-				}
-			} else {
-				//data := MustAsset("static/error.html")
-				h.logger.Println("passed empty url")
-			}
-		} else {
-			h.logger.Println("invalid path")
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		}
-	} else {
-		h.logger.Println("request method not allowed")
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+}
+
+func (h *HTTPImageProxyHandler) Stop() {
+	h.stopLock.Lock()
+	defer h.stopLock.Unlock()
+
+	if h.stopChan != nil {
+		close(h.stopChan)
+		h.stopChan = nil
 	}
-
-	log.Println("finished connection handling")
 }
--- main.go
+++ main.go
@@ -6,41 +6,44 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"runtime"
 	"time"
 
 	"gopkg.in/tylerb/graceful.v1"
 )
 
-var serverAddr string
+var ServerAddr string
 
 func init() {
-	flag.StringVar(&serverAddr, "addr", "127.0.0.1:8888", "server address")
+	flag.StringVar(&ServerAddr, "addr", "127.0.0.1:8888", "server address")
 }
 
 func main() {
 	flag.Parse()
 
+	//log.Println(net.LookupAddr(ServerAddr))
+	//log.Println(net.LookupHost(ServerAddr))
+	//log.Println(net.LookupIP(ServerAddr))
+	//return
+
 	logger := log.New(os.Stdout, "", log.LstdFlags)
 	logger.Println("initializing server")
 
-	// TODO USE MAX CPU
-
-	stopHandlingChan := make(chan struct{})
+	handler := NewHTTPImageProxyHandler(logger)
 
 	server := &graceful.Server{
 		Timeout: time.Second,
 		Server: &http.Server{
-			Addr: serverAddr,
-			Handler: &HTTPImageProxyHandler{
-				logger,
-				stopHandlingChan,
-			},
+			Addr:    ServerAddr,
+			Handler: MiddlewareSecurity(handler, logger),
 		},
 		Logger: logger,
 		ShutdownInitiated: func() {
-			close(stopHandlingChan)
+			handler.Stop()
 		},
 	}
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	logger.Println("starting server")
 
