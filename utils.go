3c8eedde83e38f6b0bd8d10a8e42fd193f13f3f3
Ivan Pushkin <pushkin13@bk.ru>
2016-05-24 01:57:07 +0300

    created util funcs

--- utils.go
+++ utils.go
@@ -1,6 +1,11 @@
 package main
 
-import "regexp"
+import (
+	"github.com/vincent-petithory/dataurl"
+	"html/template"
+	"net/http"
+	"regexp"
+)
 
 var ExpDetectDataURL = regexp.MustCompile(
 	`(?i)^\s*data:([a-z]+\/[a-z0-9\-\+]+(;[a-z\-]+\=[a-z0-9\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$`)
@@ -19,4 +24,78 @@ func IsBrowserImageContentType(contentType string) bool {
 	}
 
 	return false
+}
+
+var ErrorPageTmpl = template.Must(template.New("error_page").Parse(`<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Error</title>
+    </head>
+    <body>
+        <h1>Error</h1>
+        <p>{{.}}</p>
+    </body>
+</html>
+`))
+
+// HTTPErrorHTML sends error message with specific status code
+func HTTPErrorHTML(w http.ResponseWriter, error string, code int) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(code)
+	ErrorPageTmpl.Execute(w, error)
+}
+
+var ImagesPageTmpl = template.Must(template.New("images_page").Parse(`<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Images</title>
+    </head>
+    <body>
+        <h1>Images</h1>
+        {{range .}}<img src="{{.}}">
+        {{else}}<b>No images</b>{{end}}
+    </body>
+</html>
+`))
+
+func ImagesHTMLRender(w http.ResponseWriter, dataURLChan <-chan *dataurl.DataURL) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	ImagesPageTmpl.Execute(w, dataURLChan)
+}
+
+func MergeErrorChans(stopChan <-chan struct{}, errorChan1 <-chan error, errorChan2 <-chan error) <-chan error {
+	errorChan := make(chan error)
+
+	go func() {
+		defer close(errorChan)
+
+		select {
+		case <-stopChan:
+			return
+		case errorChan <- <-errorChan1:
+		case errorChan <- <-errorChan2:
+		}
+	}()
+
+	return errorChan
+}
+
+func MergeDataURLChans(stopChan <-chan struct{}, dataURLChan1 <-chan *dataurl.DataURL, dataURLChan2 <-chan *dataurl.DataURL) <-chan *dataurl.DataURL {
+	dataURLChan := make(chan *dataurl.DataURL)
+
+	go func() {
+		defer close(dataURLChan)
+
+		select {
+		case <-stopChan:
+			return
+		case dataURLChan <- <-dataURLChan1:
+		case dataURLChan <- <-dataURLChan2:
+		}
+	}()
+
+	return dataURLChan
 }
